14c6a184793a40b9c56485696b20743ff8df2e15
Hector Rincon <hecerinc@hotmail.com>
2015-03-21 22:39:50 -0500

    Added neccessary files for contributors

--- app/Config/database.php
+++ app/Config/database.php
@@ -72,9 +72,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'hackmty',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
